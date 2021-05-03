6128eb0f6c79f5e1e1182a6bd9a283317ab5caeb
luckyrawat565 <lrawat565>
2021-05-03 20:56:29 +0530

    commit all files

--- src/views.py
+++ src/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from django.contrib.auth.forms import UserCreationForm as user_signup
+from .forms import usersignup
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'src/home.html')
+
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = usersignup(request.POST)
+        if form.is_valid():
+            
+            form.save()
+            messages.success(request,'Your account has been created. You can login now !')
+            return redirect('profile/')  
+    else:
+        form = usersignup()
+    return render(request, 'src/signup.html',{'form':form})
+
+
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            fm = AuthenticationForm(request=request,data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request,user)
+                    return HttpResponseRedirect('../profile')
+        else:
+            fm = AuthenticationForm()
+        return render(request,'src/login.html',{'form':fm})
+    else:
+        return HttpResponseRedirect('../profile/')
+
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('../login')
+
+def user_profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'src/profile.html')
+    else:
+        return HttpResponseRedirect('../login')
+    
