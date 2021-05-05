136fc1a7c687eb23764e0ca10c91542277b08cf6
luckyrawat565 <lrawat565>
2021-05-05 20:42:12 +0530

    commit all files perfect

--- src/models.py
+++ src/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Blog(models.Model):
+    user = models.OneToOneField(User, default = 1, on_delete = models.SET_DEFAULT,primary_key = True)
+    title = models.CharField(max_length = 100)
+    desc = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.title
